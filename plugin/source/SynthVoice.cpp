5b13758b7382a358bcba0f208afdfcf0faf9edf9
Fabrizio Perria <fabrizio.perria@gmail.com>
2023-10-24 11:50:08 +0200

    Add single voice to synth

--- plugin/source/SynthVoice.cpp
+++ plugin/source/SynthVoice.cpp
@@ -3,20 +3,22 @@
 
 bool SynthVoice::canPlaySound(SynthesiserSound *sound)
 {
-	return dynamic_cast<SynthSound *>(sound) != nullptr;
+	return isPrepared && dynamic_cast<SynthSound *>(sound) != nullptr;
 }
 
 void SynthVoice::startNote(int midiNoteNumber, float velocity, SynthesiserSound *sound, int currentPitchWheelPosition)
 {
-	juce::ignoreUnused(midiNoteNumber);
 	juce::ignoreUnused(velocity);
 	juce::ignoreUnused(sound);
 	juce::ignoreUnused(currentPitchWheelPosition);
+	envelope.noteOn();
+	osc.setFrequency(static_cast<float>(juce::MidiMessage::getMidiNoteInHertz(midiNoteNumber)));
 }
 void SynthVoice::stopNote(float velocity, bool allowTailOff)
 {
 	juce::ignoreUnused(velocity);
 	juce::ignoreUnused(allowTailOff);
+	envelope.noteOff();
 }
 void SynthVoice::pitchWheelMoved(int newPitchWheelValue)
 {
@@ -27,10 +29,29 @@ void SynthVoice::controllerMoved(int controllerNumber, int newControllerValue)
 	juce::ignoreUnused(controllerNumber);
 	juce::ignoreUnused(newControllerValue);
 }
+
 void SynthVoice::renderNextBlock(AudioBuffer<float> &outputBuffer, int startSample, int numSamples)
 {
-	juce::ignoreUnused(startSample);
-	juce::ignoreUnused(numSamples);
+	jassert(isPrepared);
 	juce::dsp::AudioBlock<float> audioBlock{outputBuffer};
 	osc.process(juce::dsp::ProcessContextReplacing<float>(audioBlock));
+	gain.process(juce::dsp::ProcessContextReplacing<float>(audioBlock));
+	envelope.applyEnvelopeToBuffer(outputBuffer, startSample, numSamples);
+}
+
+void SynthVoice::prepareToPlay(double sampleRate, int samplesPerBlock, int numOutputChannels)
+{
+	dsp::ProcessSpec spec;
+	spec.sampleRate = sampleRate;
+	spec.maximumBlockSize = ( uint32_t ) samplesPerBlock;
+	spec.numChannels = ( uint32_t ) numOutputChannels;
+
+	osc.prepare(spec);
+
+	gain.setGainLinear(0.01f);
+	gain.prepare(spec);
+
+	envelope.setSampleRate(sampleRate);
+
+	isPrepared = true;
 }
